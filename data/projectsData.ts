223d74c283d4a95144d3f73482f41e280a07a509
kalanithi053 <kalanithi@amwhiz.com>
2024-09-14 17:19:32 +0530

    updated on project page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Paytm Money: Stocks, MF, IPO',
+    description: `Join the Paytm Money family today and join the 21 million+ investors who have already benefited from our services. Our all-in-one equity investment and wealth management app provides seamless access to equity investment, futures & options trading.`,
+    imgSrc: '/images/paytm.png',
+    href: 'https://apps.apple.com/in/app/paytm-money-stocks-mf-ipo/id1344431352',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Paytm: Secure UPI Paymentse',
+    description: `Send money to your friends and family using your mobile number via Paytm UPI. Including those who are not on Paytm.Scan any QR code and make payments at grocery stores, petrol pumps, restaurants etc.Recharge your mobile and pay your utility bills (electricity, gas, water, broadband etc.) easily.`,
+    imgSrc: '/images/paytm-upi.png',
+    href: 'https://apps.apple.com/us/app/paytm-secure-upi-payments/id473941634',
   },
 ]
 
