b667c03b21ef66c90bebb5c7f00261fa40df493a
kalanithi053 <kalanithi@amwhiz.com>
2024-09-16 09:29:39 +0530

    udpated

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -74,7 +74,7 @@ export default function ListLayoutWithTags({
   const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
 
   const displayPosts = initialDisplayPosts.length > 0 ? initialDisplayPosts : posts
-const tilts=title?.replaceAll("-"," ")
+  const tilts = title?.replaceAll('-', ' ')
   return (
     <>
       <div>
