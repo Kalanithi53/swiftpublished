6f4b5ce8b0eb6f0f9d8a34ba262e4dbcc91ea793
kalanithi053 <kalanithi@amwhiz.com>
2024-10-07 09:27:18 +0530

    added analtics

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-
+import { Analytics as Analytic } from "@vercel/analytics/react"
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
   display: 'swap',
@@ -82,6 +82,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-[#161615] dark:text-white">
         <ThemeProviders>
+          <Analytic />
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
